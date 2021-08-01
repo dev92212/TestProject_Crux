c04eebf359c84d090597058dd1c60228f4fb613c
dev92212 <88285458+dev92212@users.noreply.github.com>
2021-08-01 23:53:18 +0530

    Update MainTest.py

--- MainTest.py
+++ MainTest.py
@@ -8,45 +8,41 @@ class Logintest(unittest.TestCase):
     @classmethod
     def setUp(cls):
         cls.driver = webdriver.Chrome("D:/WebDrivers/chromedriver.exe")
-        cls.driver.implicitly_wait(100)
+        cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
         cls.driver.get("https://www.cruxintelligence.com")
-        cls.driver.implicitly_wait(1000)
+        cls.driver.implicitly_wait(10)
         # ACCEPT ALL COOKIES
         cls.driver.find_element_by_id("hs-eu-confirmation-button").click()
 
-    def test_Login_check(self):    ####---> click on login -> check if logging in. save the error message
+    def test_Login_check(self):    ####---> click on login -> check if logging in. Confirm the error message
+        self.driver.implicitly_wait(30)
         self.driver.find_element_by_xpath("//a[@title='LOG IN']").click()
-        time.sleep(3)
-
-        userName = self.driver.find_element_by_xpath("//input[@id='userInput']")
-        userName.clear()
-        userName.send_keys("devmishra")
+        self.driver.find_element_by_xpath("//input[@id='userInput']").clear()
+        self.driver.find_element_by_xpath("//input[@id='userInput']").send_keys("devmishra")
         self.driver.find_element_by_xpath("//div[@class='login-action']").click()
-        time.sleep(3)
         assert self.driver.find_element_by_xpath("//div[@class='login__error']").is_displayed(), "Error Message Not Displayed! Testcase Failed."
+        self.driver.close()
 
-        # if self.driver.find_element_by_xpath("//div[@class='login__error']").is_displayed() and self.driver.find_element_by_xpath("//div[@class='login__error']").is_enabled():
-        #     print("Login Failed. Error message is present. Testcase Passed.")
-        # else:
-        #     print("Error message is not present. Testcase Failed.")
-
-    def test_HompageButton(self):   ####---> Go back to homepage by clicking on logo
+    def test_HompageButton(self):   ####---> Go to homepage by clicking on logo
+        self.driver.implicitly_wait(30)
         self.driver.find_element_by_class_name("img-fluid").click()
+        if self.driver.title == "Augmented Business Intelligence | Crux Intelligence":
+            print("Reached Homepage. Testcase passed.")
+        else:
+            print("Did not reach Homepage. Testcase failed.")
+        self.driver.close()
 
-    def test_Youtube_Video(self):   ####---> Click on the video of homepage and play it for 10 seconds then stop it.
+    def test_Youtube_Video(self):   ####---> Click on the youtube video on homepage and play it.
+        self.driver.implicitly_wait(30)
         self.driver.find_element_by_xpath("//ol[@class='carousel-indicators']/li[1]").click()
         self.driver.find_element_by_xpath("//div[@class='d-home21-vdo']/div/iframe[1]").click()
-        time.sleep(20)
-        self.driver.find_element_by_xpath("//div[@class='d-home21-vdo']/div/iframe[1]").click()
-        time.sleep(3)
+        self.driver.close()
 
-    def test_AboutUs_NameCheck(self):   ####---> Go to about us -> check if Sumith Balagangadharan and Ganesh Subramanian name's are displayed correctly
+    def test_AboutUs_NameCheck(self):   ####---> Go to about us -> check if Sumith Balagangadharan and Ganesh Subramanian names are displayed correctly.
+        self.driver.implicitly_wait(30)
         self.driver.find_element_by_xpath("//a[@title='About Us']").click()
-        time.sleep(3)
-
-        if self.driver.find_element_by_link_text(
-                "Sumith Balagangadharan").is_displayed() and self.driver.find_element_by_link_text("Sumith Balagangadharan").is_enabled():
+        if self.driver.find_element_by_link_text("Sumith Balagangadharan").is_displayed() and self.driver.find_element_by_link_text("Sumith Balagangadharan").is_enabled():
             print("NAME MATCHES! Sumith Balagangadharan is present. Testcase Passed.")
         else:
             print("NAME DID NOT MATCH! Testcase Failed.")
@@ -55,11 +51,10 @@ class Logintest(unittest.TestCase):
             print("NAME MATCHES! Ganesh Subramanian is present. Testcase Passed.")
         else:
             print("NAME DID NOT MATCH! Testcase Failed.")
+        self.driver.close()
 
-        # assert driver.find_element_by_link_text("Sumith Balagangadharan").is_displayed(), "NAME DID NOT MATCH! Testcase Failed."
-        # assert driver.find_element_by_link_text("Ganesh Subramanian").is_displayed(), "NAME DID NOT MATCH! Testcase Failed."
-
-    def test_PlaystoreButton(self):    ####---> Check if opened in new tab when clicked on playstore and apple store buttons
+    def test_PlaystoreButton(self):    ####---> Check if opened in new tab when clicked on playstore and applestore buttons.
+        self.driver.implicitly_wait(30)
         playStore = self.driver.find_element_by_xpath("//div[@class='col-md-6']/a[1]/img")
         assert playStore.is_enabled(), "LINK DID NOT OPEN IN NEW TAB!"
         playStore.click()
@@ -75,8 +70,8 @@ class Logintest(unittest.TestCase):
         self.driver.switch_to.window(self.driver.window_handles[1])
         self.driver.close()
         self.driver.switch_to.window(self.driver.window_handles[-1])
+        self.driver.close()
 
     @classmethod
     def tearDownClass(cls):
-        #cls.driver.delete_all_cookies()
         cls.driver.quit()
